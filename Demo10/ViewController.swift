177cfd5e0884a9651f7d438a30f2e5fbf7d70025
mjz <mctzeng@gmail.com>
2019-11-19 15:21:29 +0800

    change view bg color

--- Demo10/ViewController.swift
+++ Demo10/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
         // Do any additional setup after loading the view.
     }
 
